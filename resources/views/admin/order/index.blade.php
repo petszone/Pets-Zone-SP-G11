07892ba7c8b1a77020119fa685e10f36ef6b4b0f
macbook <macbook@macbooks-MacBook-Pro.local>
2023-02-19 02:22:43 +0200

    edit

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -12,20 +12,7 @@
                     <!--begin::Page Heading-->
                     <div class="d-flex align-items-baseline flex-wrap mr-5">
                         <!--begin::Page Title-->
-                        {{-- <h5 class="text-dark font-weight-bold my-1 mr-5">طلبات الإفادة</h5> --}}
-                        <!--end::Page Title-->
-                        <!--begin::Breadcrumb-->
-                        {{-- <ul class="breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold p-0 my-2 font-size-sm">
-                            <li class="breadcrumb-item text-muted">
-                                <a href="" class="text-muted">Features</a>
-                            </li>
-                            <li class="breadcrumb-item text-muted">
-                                <a href="" class="text-muted">All</a>
-                            </li>
-                            <li class="breadcrumb-item text-muted">
-                                <a href="" class="text-muted">Users List</a>
-                            </li>
-                        </ul> --}}
+                 
                         <!--end::Breadcrumb-->
                     </div>
                     <!--end::Page Heading-->
